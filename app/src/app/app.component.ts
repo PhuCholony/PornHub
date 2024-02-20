6bd0e3e972e7c21e80cae9a5cb8d2417dfb2d8b4
Phuchon Chueapong <128043114+PhuCholony@users.noreply.github.com>
2024-02-20 19:19:01 +0700

    setup pornhub application

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterOutlet],
   selector: 'phd-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'pornhub';
+  constructor() {}
 }

8d188c186f3b1f637e94cbf04f5eb303f82daf6c
Phuchon Chueapong <128043114+PhuCholony@users.noreply.github.com>
2024-02-20 19:49:13 +0700

    update pornhub application
    Create home page and about page

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+];
